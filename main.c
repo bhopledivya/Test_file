fee21b81043add5957a73f9d4b326e390e834ece
bhopledivya <59743644+bhopledivya@users.noreply.github.com>
2020-01-14 19:22:05 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include<conio.h>
 main()
 {
- int mul[10][10];
+ int mul[10][10],i,j;
  printf("\n Multiplication table:\n");
- for(int i=0;i<10;i++)
+ for(i=0;i<10;i++)
  {
-   for(int j=0;j<10;j++)
+   for(j=0;j<10;j++)
    {
      mul[i][j]=(i+1)*(j+1);
    }
  }
-  for(int i=0;i<10;i++)
+  for(i=0;i<10;i++)
   {
-   for(int j=0;j<10;j++)
+   for(j=0;j<10;j++)
    {
      printf("%4d",mul[i][j]);
    }
